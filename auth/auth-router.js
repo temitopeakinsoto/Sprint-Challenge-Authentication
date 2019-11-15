ec5c99d04c256b30b5266a02dc7a59bbafc4d71f
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-15 09:01:41 +0100

    Create [POST] /api/auth/register endpoint for registering a new user to the datbase

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
+const Users = require('./helpers');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const { username, password } = req.body;
+  const user = {username, password}
+  Users.add(user)
+  .then(saved => {
+    res.status(201).json({message: "it worked!"})
+  })
+  .catch(err => {
+    res.status(500).json({message: "It did not work!" + err.message})
+  })
 });
 
 router.post('/login', (req, res) => {
