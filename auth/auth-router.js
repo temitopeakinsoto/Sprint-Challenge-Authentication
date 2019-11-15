e6d72d4a93562984bde5168a142e588aaeaf96d0
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-15 10:23:04 +0100

    Implement the login endpoint functionality
    In this commit, I implemented the [POST] /auth/login endpoint.
    this endpoint logs a user in by using bcrypt.compareSync to compare the password stored in the db against the user-supplied passed.
    upon successful login, a token is generated and sent backe user.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Users = require('./helpers');
+const secret = process.env.JWT_SECRET;
 
 router.post('/register', (req, res) => {
   const { username, password } = req.body;
@@ -21,8 +22,38 @@ router.post('/register', (req, res) => {
   })
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+  
+  Users.findByUser(username)
+  .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials"  });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({message: `There was an error with this operatio: ${error.message}`});
+    });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+
+  const result = jwt.sign(payload, secret, options);
+
+  return result;
+}
 
 module.exports = router;
